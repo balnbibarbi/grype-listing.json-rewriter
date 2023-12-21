e87ca01890522c556d3c1ab0d584fa2bc47cd80b
someguy <nvienlvnwlnw@mailinator.com>
2023-12-21 14:25:51 +1100

    Placate pylint and pycodestyle.

--- tests.py
+++ tests.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 
+"""
+Unit tests for the Listing module.
+"""
+
 import unittest
 import json
 from listing import Listing
@@ -8,12 +12,27 @@ from listing import Listing
 
 class TestListing(unittest.TestCase):
 
+
+    """
+    Test the Listing class.
+    """
+
     def test_minimise(self):
+        """
+        Test loading and minimising the listing.
+        """
         actual_listing = Listing("tests/input.json", True)
-        with open("tests/expected-output.json", "r") as expected_output:
+        with open(
+            "tests/expected-output.json",
+            "r",
+            encoding='utf-8'
+        ) as expected_output:
             expected_listing = json.load(expected_output)
             # The URLs are expected to differ, so we nuke them
-            for data in actual_listing.listing['available'], expected_listing['available']:
+            for data in (
+                actual_listing.listing['available'],
+                expected_listing['available']
+            ):
                 for schema in data.values():
                     for version in schema:
                         version['url'] = 'expected to differ'
